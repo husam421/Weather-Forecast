43bf1e2f61c6c86786d01443e3bed5f6523ebe36
Omar Mohammed <v0l0b@ece.ubc.ca>
2017-06-18 23:38:46 -0400

    Added something something

--- hussam.py
+++ hussam.py
@@ -5,3 +5,4 @@ import pandas as pd #structure the downloaded data
 result = requests.get('https://www.theweathernetwork.com/ca/weather/british-columbia/burnaby')
 soup = bs(result.content, 'html.parser')
 print('hello')
+print('Hi hussam')
